ab788b03d2e2932419030444e8bc4a70fdbb3f11
Mariia Ros <rosm0099@gmail.com>
2023-04-20 02:19:57 +0200

    commit

---
+++ js/modal.js
@@ -0,0 +1,36 @@
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector('[data-modal-open]'),
+    closeModalBtn: document.querySelector('[data-modal-close]'),
+    modal: document.querySelector('[data-modal]'),
+  };
+
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle('is-hidden');
+  }
+})();
+
+(() => {
+  document.querySelector('.js-speaker-form').addEventListener('submit', e => {
+    e.preventDefault();
+
+    new FormData(e.currentTarget).forEach((value, name) =>
+      console.log(`${name}: ${value}`)
+    );
+
+    e.currentTarget.reset();
+  });
+})();
+
+{
+  /* <script>
+      const { height: pageHeaderHeight } = document
+      .querySelector (".page-header")
+      .getBoundingClientRect();
+
+      document.body.style.paddingTop = `${pageHeaderHeight}px`;
+    </script> */
+}
